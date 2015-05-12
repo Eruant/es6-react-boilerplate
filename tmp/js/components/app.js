1718084e8338ed30451789321dbb7fc4fa5be80a
Matt Gale <matt@littleball.co.uk>
2015-05-12 17:42:34 +0100

    Added basic files for react > es6 > browserify tranform

---
+++ tmp/js/components/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var App = (function (_React$Component) {
+    function App() {
+        _classCallCheck(this, App);
+
+        if (_React$Component != null) {
+            _React$Component.apply(this, arguments);
+        }
+    }
+
+    _inherits(App, _React$Component);
+
+    _createClass(App, [{
+        key: 'render',
+        value: function render() {
+            return _react2['default'].createElement(
+                'h1',
+                null,
+                'I\'m from the UK, uk, Uk, uK'
+            );
+        }
+    }]);
+
+    return App;
+})(_react2['default'].Component);
+
+exports['default'] = App;
+module.exports = exports['default'];
---
+++ tmp/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _componentsApp = require('./components/app');
+
+var _componentsApp2 = _interopRequireDefault(_componentsApp);
+
+_react2['default'].render(_react2['default'].createElement(_componentsApp2['default'], null), document.getElementById('main'));
